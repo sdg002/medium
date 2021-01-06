ccbed73c6e134a842d4ac49780d671dc04babdf4
saurabh <saurabh_dasgupta@hotmail.com>
2021-01-06 10:22:39 +0000

    Added multiple colors, parameterized the min inlier count

--- recursive-ransac/src/RansacMultipleLines.py
+++ recursive-ransac/src/RansacMultipleLines.py
@@ -8,8 +8,7 @@ import math
 import datetime
 
 min_samples=3 #RANSAC parameter - The minimum number of data points to fit a model to.
-distance_from_line=1 #RANSAC parameter - Maximum distance for a data point to be classified as an inlier.
-min_inliers_allowed=5 #Custom parameter  - A line is selected only if these many inliers are found
+#min_inliers_allowed=5 #Custom parameter  - A line is selected only if these many inliers are found
 
 def read_black_pixels(imagefilename:str):
     #returns a numpy array with shape (N,2) N points, x=[0], y=[1]
@@ -31,13 +30,13 @@ def read_black_pixels(imagefilename:str):
     np_data_points=np.column_stack((indices[1],cartesian_y)) 
     return np_data_points, width,height
 
-def extract_first_ransac_line(data_points:[]):
+def extract_first_ransac_line(data_points:[], max_distance:int):
     #Accepts a numpy array with shape N,2  N points, with coordinates x=[0],y=[1]
     #Returns a numpy array with shape (N,2), these are the inliers of the just discovered ransac line
     
     
     model_robust, inliers = ransac(data_points, LineModelND, min_samples=min_samples,
-                                   residual_threshold=distance_from_line, max_trials=1000)
+                                   residual_threshold=max_distance, max_trials=1000)
     results_inliers=[]
     results_inliers_removed=[]
     for i in range(0,len(data_points)):
@@ -50,30 +49,33 @@ def extract_first_ransac_line(data_points:[]):
         results_inliers.append((x,y))
     return np.array(results_inliers), np.array(results_inliers_removed)
 
-# def remove_inliers_from_image_points(src_points,inlier_points):
-#     #Removes all inlier points from the src array
-#     #Returns a numpy array with shape (N,2)
-#     return None
 
 def superimpose_all_inliers(points_lists,width:float, height:float):
     #Create an RGB image array with dimension heightXwidth
     #Draw the points with various colours
     #return the array
+
     new_image=np.full([height,width,3],255,dtype='int')
-    red=int(0)
-    green=int(255)
-    blue=int(0)
-    for points_array in points_lists:
+    colors=[(0,255,0),(255,255,0),(0,0,255)]
+    for points_index in range(0,len(points_lists)):
+        color=colors[points_index % len(colors)]
+        points_array=points_lists[points_index]
         for point in points_array:
             x=point[0]
             y=point[1]
             new_y=height-y-1
-            new_image[new_y][x][0]=red
-            new_image[new_y][x][1]=green
-            new_image[new_y][x][2]=blue
+            new_image[new_y][x][0]=color[0]
+            new_image[new_y][x][1]=color[1]
+            new_image[new_y][x][2]=color[2]
     return new_image
 
-def extract_multiple_lines_and_save(inputfilename:str,iterations:int):
+def extract_multiple_lines_and_save(inputfilename:str,iterations:int, max_distance:int,min_inliers_allowed:int):
+    """
+    min_inliers_allowed - a line is selected only if it has more than this inliers. The search process is halted when this condition is met
+    max_distance - This is the RANSAC threshold distance from a line for a point to be classified as inlier
+    """
+    print("---------------------------------------")
+    print("Processing: %s" % (inputfilename))
     results=[]
     all_black_points,width,height=read_black_pixels(inputfilename)
     starting_points=all_black_points
@@ -81,7 +83,7 @@ def extract_multiple_lines_and_save(inputfilename:str,iterations:int):
         if (len(starting_points) <= min_samples):
             print("No more points available. Terminating search for RANSAC")
             break
-        inlier_points,inliers_removed_from_starting=extract_first_ransac_line(starting_points)
+        inlier_points,inliers_removed_from_starting=extract_first_ransac_line(starting_points,max_distance=max_distance)
         if (len(inlier_points) < min_inliers_allowed):
             print("Not sufficeint inliers found %d , threshold=%d, therefore halting" % (len(inlier_points),min_inliers_allowed))
             break
@@ -99,5 +101,9 @@ def extract_multiple_lines_and_save(inputfilename:str,iterations:int):
 
 
 
-extract_multiple_lines_and_save("SmallCross.png",5)
-extract_multiple_lines_and_save("SmallCrossWithNoise.png",5)
+# extract_multiple_lines_and_save("SmallCross.png",5)
+# extract_multiple_lines_and_save("SmallCrossWithNoise.png",5)
+# extract_multiple_lines_and_save("2ProminentLine.png",5)
+#todo some problem in one of the above lines, excpetion
+extract_multiple_lines_and_save("2ProminentLineWithNoise.png",5,max_distance=3, min_inliers_allowed=5)
+extract_multiple_lines_and_save("3ProminentLineWithNoise.png",5, max_distance=3,min_inliers_allowed=5)
