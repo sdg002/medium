8101e7c2a49bb9265f48a4d78d71ea1653dd0ac4
saurabh <saurabh_dasgupta@hotmail.com>
2021-01-06 13:01:59 +0000

    Added RansacLineInfo class

--- recursive-ransac/src/RansacMultipleLines.py
+++ recursive-ransac/src/RansacMultipleLines.py
@@ -1,5 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from typing import Union, Any, List, Optional, cast, Dict
 
 from skimage.measure import LineModelND, ransac
 import os
@@ -9,6 +10,13 @@ import datetime
 
 min_samples=3 #RANSAC parameter - The minimum number of data points to fit a model to.
 #min_inliers_allowed=5 #Custom parameter  - A line is selected only if these many inliers are found
+
+class RansacLineInfo(object):
+    """Helper class to manage the information about the RANSAC line."""
+    def __init__(self, inlier_points:np.ndarray, model:LineModelND):
+        self.inliers=inlier_points #the inliers that were detected by RANSAC algo
+        self.model=model    #The LinearModelND that was a result of RANSAC algo
+        
 
 def read_black_pixels(imagefilename:str):
     #returns a numpy array with shape (N,2) N points, x=[0], y=[1]
@@ -29,8 +37,13 @@ def read_black_pixels(imagefilename:str):
     return np_data_points, width,height
 
 def extract_first_ransac_line(data_points:[], max_distance:int):
-    #Accepts a numpy array with shape N,2  N points, with coordinates x=[0],y=[1]
-    #Returns a numpy array with shape (N,2), these are the inliers of the just discovered ransac line
+    """
+    Accepts a numpy array with shape N,2  N points, with coordinates x=[0],y=[1]
+    Returns 
+         A numpy array with shape (N,2), these are the inliers of the just discovered ransac line
+         All data points with the inliers removed
+         The model line
+    """
     
     
     model_robust, inliers = ransac(data_points, LineModelND, min_samples=min_samples,
@@ -45,20 +58,20 @@ def extract_first_ransac_line(data_points:[], max_distance:int):
         x=data_points[i][0]
         y=data_points[i][1]
         results_inliers.append((x,y))
-    return np.array(results_inliers), np.array(results_inliers_removed)
+    return np.array(results_inliers), np.array(results_inliers_removed),model_robust
 
 
-def superimpose_all_inliers(points_lists,width:float, height:float):
+def superimpose_all_inliers(ransac_lines,width:float, height:float):
     #Create an RGB image array with dimension heightXwidth
     #Draw the points with various colours
     #return the array
 
     new_image=np.full([height,width,3],255,dtype='int')
     colors=[(0,255,0),(255,255,0),(0,0,255)]
-    for points_index in range(0,len(points_lists)):
-        color=colors[points_index % len(colors)]
-        points_array=points_lists[points_index]
-        for point in points_array:
+    for line_index in range(0,len(ransac_lines)):
+        color=colors[line_index % len(colors)]
+        ransac_lineinfo:RansacLineInfo=ransac_lines[line_index]
+        for point in ransac_lineinfo.inliers:
             x=point[0]
             y=point[1]
             new_y=height-y-1
@@ -77,7 +90,7 @@ def extract_multiple_lines_and_save(inputfilename:str,iterations:int, max_distan
     folder_script=os.path.dirname(__file__)
     absolute_path=os.path.join(folder_script,"images/",inputfilename)
 
-    results=[]
+    results:List[RansacLineInfo]=[]
     all_black_points,width,height=read_black_pixels(absolute_path)
     print("Found %d pixels in the file %s" % (len(all_black_points),inputfilename))
     starting_points=all_black_points
@@ -85,12 +98,12 @@ def extract_multiple_lines_and_save(inputfilename:str,iterations:int, max_distan
         if (len(starting_points) <= min_samples):
             print("No more points available. Terminating search for RANSAC")
             break
-        inlier_points,inliers_removed_from_starting=extract_first_ransac_line(starting_points,max_distance=max_distance)
+        inlier_points,inliers_removed_from_starting,model=extract_first_ransac_line(starting_points,max_distance=max_distance)
         if (len(inlier_points) < min_inliers_allowed):
             print("Not sufficeint inliers found %d , threshold=%d, therefore halting" % (len(inlier_points),min_inliers_allowed))
             break
         starting_points=inliers_removed_from_starting
-        results.append(inlier_points)
+        results.append(RansacLineInfo(inlier_points,model))
         print("Found %d RANSAC lines" % (len(results)))
     superimposed_image=superimpose_all_inliers(results,width,height)
     #Save the results
